34046d2d9ef0d54c4db2fb4c5fda6fdaab46aa2b
madalinazanficu <zanficumadalina@gmail.com>
2024-01-06 18:05:22 +0200

    Added influxdb service

--- tema3/adapter.py
+++ tema3/adapter.py
@@ -1,6 +1,7 @@
 import paho.mqtt.client as mqtt
 import time
 import re
+import json
 import influxdb_client
 from influxdb_client.client.write_api import SYNCHRONOUS
 
@@ -20,16 +21,49 @@ def on_message(client, userdata, msg):
     pattern = r'^[^/]+/[^/]+$'
     if re.match(pattern, msg.topic) == False:
         return
+    print("Received a message by topic [" + msg.topic + "]")
+
+
+    # Extract entities from the topic and payload
+    location = msg.topic.split("/")[0]
+    station = msg.topic.split("/")[1]
+    payload = json.loads(msg.payload.decode())
+
+
+    # Check if payload contains timestamp or generate it
+    if "timestamp" not in payload:
+        timestamp = time.strftime("%Y-%m-%dT%H:%M:%S%z")
+    else:
+        timestamp = payload["timestamp"]
+
+    # Create a time series for each numeric entry in the payload
+    for key, value in payload.items():
+        if type(value) == int or type(value) == float:
+            measurement = location + "." + station + "." + key
+            print("Measurement: " + measurement)
+            point = influxdb_client.Point(measurement) \
+                        .tag("location", location) \
+                        .tag("station", station) \
+                        .field("value", value) \
+                        .time(timestamp)
+            
+            print("Crapa dupa point")
+            write_api = db_client.write_api(write_options=SYNCHRONOUS)
+            write_api.write(bucket="weather_station", record=point)
+            print("Crapa dupa write")
+            print(measurement + " -> " + str(value) + " at " + timestamp)
     
-    print("Received message: " + msg.topic + " -> " + msg.payload.decode())
 
 
 if __name__ == "__main__":
-    # set callbacks
-    global client
-    client = mqtt.Client()
-    client.on_connect = on_connect
-    client.on_message = on_message
+    # Create the mqtt client (which listens for messages) and set the callbacks
+    mqtt_client = mqtt.Client()
+    mqtt_client.on_connect = on_connect
+    mqtt_client.on_message = on_message
 
-    client.connect("mqtt-broker", 1883, 60)
-    client.loop_forever()
+    mqtt_client.connect("mqtt-broker", 1883, 60)
+    mqtt_client.loop_forever()
+
+    # Create the influxdb client - using InfluxDB 1.x in order to avoid autehntication
+    db_client = influxdb_client.InfluxDBClient(host="influxdb", port=8086)
+    db_client.create_database("weather_station")
